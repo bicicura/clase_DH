785555e4dfdefe9935b24427c60ec09715f55a2a
Agustin <agustinlalia1@gmail.com>
2019-11-08 16:07:10 -0300

    Termine la pagina 2 y empece la pagina 3

--- js/pag2.js
+++ js/pag2.js
@@ -1,7 +1,21 @@
 window.addEventListener ('load', function() {
   var listadoURL = 'https://api.themoviedb.org/3/genre/tv/list?api_key=b0f40cf877bd4ccb9f9f1975eb5ffa65&language=es-AR'
+  var list = document.querySelector('.listado')
 
+  fetch(listadoURL)
+	.then(function (respuesta){
+		return respuesta.json();
+	})
+	.then(function (informacion){
+    console.log(informacion)
+    var id = informacion.id
+  	for (var genero of informacion.genres) {
+			list.innerHTML += `<li><a href="pagina3.html?${id}">` + genero.name + `</a></li>`;
+		}
+    // console.log(informacion);
+	})
+	.catch(function (error) {
+		console.log("Error: " + error);
+	})
 
-
-
-}
+})
