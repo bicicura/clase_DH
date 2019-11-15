ed660d3e1a7a03f53eb769ee1d12aa6fb972de10
ffff <fr4ncolamattina@gmail.com>
2019-11-15 10:54:34 -0300

    Cambio pag 4

--- js/pag4.js
+++ js/pag4.js
@@ -7,4 +7,14 @@ window.addEventListener('load',function(){
   var url = "https://api.themoviedb.org/3/search/company?api_key=b0f40cf877bd4ccb9f9f1975eb5ffa65&query=querystring" + queryString + "&page=1";
 
 fetch(url)
+.then(function(respuesta){
+  return respuesta.json()
+})
+.then(function(resultado) {
+  console.log (resultado);
+
+})
+.catch (function (errores){
+  console.log(errores);
+})
 }
