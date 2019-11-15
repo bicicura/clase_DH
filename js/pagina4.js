a2bdb0a43091a4aa8d75d527faad2a1f8ae14644
ffff <fr4ncolamattina@gmail.com>
2019-11-15 10:30:51 -0300

    inicio del buscador

---
+++ js/pagina4.js
@@ -0,0 +1,10 @@
+window.addEventListener('load',function(){
+  var URLParams = new URLSearchParams(window.location.search);
+  var queryString = URLParams.get('buscador');
+
+  console.log(queryString);
+
+  var url = "https://api.themoviedb.org/3/search/company?api_key=b0f40cf877bd4ccb9f9f1975eb5ffa65&query=querystring" + queryString + "&page=1";
+
+fetch(url)
+}
